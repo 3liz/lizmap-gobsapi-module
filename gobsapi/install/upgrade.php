55726aaad6bb994fb8ec06f75728f8908cd2b8f5
Michael Douchin <mdouchin@3liz.com>
2023-07-25 14:27:51 +0200

    Installation & Upgrade - Ensure the entry point gobsapi.php is overwritten

--- gobsapi/install/upgrade.php
+++ gobsapi/install/upgrade.php
@@ -15,10 +15,45 @@ class gobsapiModuleUpgrader extends jInstallerModule
         // Needed in the upgrade process
         // if the variable $mapping has changed
         if (method_exists($this, 'createEntryPoint')) {
-            $this->createEntryPoint('gobsapi.php', 'gobsapi/config.ini.php', 'gobsapi', 'classic');
+            if (!file_exists(jApp::wwwPath('gobsapi.php'))) {
+                $this->createEntryPoint('gobsapi.php', 'gobsapi/config.ini.php', 'gobsapi', 'classic');
+            } else {
+                $modulePath = jApp::getModulePath('gobsapi');
+                $sourceFile = $modulePath.'/install/gobsapi.php';
+                $this->myUpdateEntryPointFile($sourceFile);
+            }
         } else {
             // deprecated and not safe
-            $this->copyFile('gobsapi.php', jApp::wwwPath('gobsapi.php'));
+            $overwrite = true;
+            $this->copyFile('gobsapi.php', jApp::wwwPath('gobsapi.php'), $overwrite);
         }
+    }
+
+    /**
+     * Update the entry point (overwrite the file
+     * and adapt application init file
+     *
+     * @param string $entryPointFile Entry point file
+     */
+    protected function myUpdateEntryPointFile($entryPointFile)
+    {
+        $entryPointFileName = basename($entryPointFile);
+        $epPath = jApp::wwwPath($entryPointFileName);
+        if (!file_exists($epPath)) {
+            throw new \Exception('The entrypoint '.$entryPointFile.' cannot be updated, as it doesn\'t exist');
+        }
+
+        // copy the entrypoint and its configuration
+        $this->copyFile($entryPointFile, $epPath, true);
+
+        // change the path to application.init.php into the entrypoint
+        // depending on the application, the path of www/ is not always at the same place, relatively to
+        // application.init.php
+        $appInitFile = jApp::applicationInitFile();
+        $relativePath = \Jelix\FileUtilities\Path::shortestPath(jApp::wwwPath(), dirname($appInitFile).'/');
+
+        $epCode = file_get_contents($epPath);
+        $epCode = preg_replace('#(require\s*\(?\s*[\'"])(.*)(application\.init\.php)([\'"])#m', '\\1'.$relativePath.'/'.basename($appInitFile).'\\4', $epCode);
+        file_put_contents($epPath, $epCode);
     }
 }
